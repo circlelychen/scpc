ced2698f4c69a0650f98046c5e6b1edb74c9aebe
circlelychen <truecirclely@gmail.com>
2020-10-07 22:03:50 +0800

    change volume path for run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-cmd="docker run --rm -it -v $(pwd):/scpc scpc:latest python crawl.py"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cmd="docker run --rm -it -v $DIR:/scpc scpc:latest python crawl.py"
 echo $cmd
 eval $cmd
